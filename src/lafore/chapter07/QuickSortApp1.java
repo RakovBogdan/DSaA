891acb62856c4a9c81d9563ec1b19b264da75a3d
RakovBogdan <rakovbogdan@gmail.com>
2018-05-19 13:42:38 +0300

    QuickSort implemented

--- src/lafore/chapter07/QuickSortApp1.java
+++ src/lafore/chapter07/QuickSortApp1.java
@@ -24,6 +24,50 @@ class ArrayQuick1 {
         }
         System.out.println();
     }
+
+    public void quickSort() {
+        recursiveQuickSort(0, numberOfElements - 1);
+    }
+
+    private void recursiveQuickSort(int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        long pivot = values[right];
+        int pivotPosition = partition(left, right, pivot);
+
+        recursiveQuickSort(left, pivotPosition - 1);
+        recursiveQuickSort(pivotPosition + 1, right);
+    }
+
+    private int partition(int left, int right, long pivot) {
+
+        int leftIndex = left - 1;
+        int rightIndex = right;
+
+        while (true) {
+            while (values[++leftIndex] < pivot) ;
+
+            while (values[--rightIndex] > pivot && rightIndex > left) ;
+
+            if (leftIndex >= rightIndex) {
+                break;
+            } else {
+                swap(leftIndex, rightIndex);
+            }
+        }
+
+        swap(leftIndex, right);
+
+        return leftIndex;
+    }
+
+    private void swap(int firstIndex, int secondIndex) {
+        long tmp = values[firstIndex];
+        values[firstIndex] = values[secondIndex];
+        values[secondIndex] = tmp;
+    }
 }
 
 public class QuickSortApp1 {
@@ -36,6 +80,8 @@ public class QuickSortApp1 {
             arrayQuick1.addElement((long)(Math.random() * maxSize));
         }
 
+        arrayQuick1.display();
+        arrayQuick1.quickSort();
         arrayQuick1.display();
     }
 }
